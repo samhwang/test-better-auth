bf39caf703d8a32461bfbf6f70ddadea14ae90d7
Sam Huynh <sam.huynh@lyka.com.au>
2025-01-01 23:58:29 +1100

    feat: setup prisma serverless

--- server/auth/server.ts
+++ server/auth/server.ts
@@ -3,7 +3,7 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { prisma } from '../prisma';
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma(), {
+  database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
   emailAndPassword: {
