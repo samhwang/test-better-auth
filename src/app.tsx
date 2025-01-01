b0d569989814dfe54293f1a31b7ae4e74aef5164
Sam Huynh <sam.huynh@lyka.com.au>
2025-01-01 19:50:27 +1100

    feat: load devtools

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
 import { httpBatchLink } from '@trpc/client';
-import { useState } from 'react';
+import { lazy, useState } from 'react';
 import { trpc } from '../server/trpc/client';
 import { appRouter } from './router';
+
+const TanstackRouterDevtools = import.meta.env.PROD
+  ? () => null
+  : lazy(() =>
+      import('@tanstack/router-devtools').then((res) => {
+        return { default: res.TanStackRouterDevtools };
+      })
+    );
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
@@ -21,6 +30,8 @@ function App() {
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={appRouter} />
+        <TanstackRouterDevtools router={appRouter} />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </trpc.Provider>
   );
