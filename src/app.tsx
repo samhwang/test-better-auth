4ecd639e145fc8bdebc6a603462f8103f37f4e30
Sam Huynh <samhwang2112.dev@gmail.com>
2025-10-23 00:04:41 +1100

    feat: update tanstack router devtools package

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { httpBatchLink } from '@trpc/client';
-import { lazy, useState } from 'react';
+import { useState } from 'react';
 import { trpc } from '../server/trpc/client';
 import { appRouter } from './router';
-
-const TanstackRouterDevtools = import.meta.env.PROD
-  ? () => null
-  : lazy(() =>
-      import('@tanstack/router-devtools').then((res) => {
-        return { default: res.TanStackRouterDevtools };
-      })
-    );
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
@@ -30,7 +23,7 @@ function App() {
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={appRouter} />
-        <TanstackRouterDevtools router={appRouter} />
+        <TanStackRouterDevtools router={appRouter} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </trpc.Provider>
