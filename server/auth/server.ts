4cb516525c98db1cf1e88db6a7c2cb2e6d2755bd
Sam Huynh <samhwang2112.dev@gmail.com>
2025-10-22 18:53:27 +1100

    chore: try to fix prisma

--- server/auth/server.ts
+++ server/auth/server.ts
@@ -2,7 +2,7 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import type { getDbClient } from '../db';
 
-export function createAuth(db: ReturnType<typeof getDbClient>) {
+export function createAuth(db: ReturnType<typeof getDbClient>): ReturnType<typeof betterAuth> {
   return betterAuth({
     database: prismaAdapter(db, {
       provider: 'postgresql',
--- server/db.ts
+++ server/db.ts
@@ -2,11 +2,11 @@ import dotenv from '@dotenvx/dotenvx';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaPg } from '@prisma/adapter-pg';
 import type { Env } from './context';
-import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from './generated/prisma/client';
 
 dotenv.config({ path: '../.dev.vars' }); // or .env.local
 
-export function getDbClient(connectionString: string) {
+export function getDbClient(connectionString: string): PrismaClient {
   const adapter = process.env.ENV === 'development' ? new PrismaPg({ connectionString }) : new PrismaNeon({ connectionString });
   const db = new PrismaClient({ adapter });
 
